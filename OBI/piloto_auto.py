c0bff4958791d4a971ff9615518d05de3b5633eb
Ana Cristina <68512242+momoyo-droid@users.noreply.github.com>
2021-10-17 11:55:12 -0300

    Update piloto_auto.py

--- OBI/piloto_auto.py
+++ OBI/piloto_auto.py
@@ -1,7 +1,7 @@
 # Programa que calcula a distância da traseira de um carro para outro.
 # caso o carro precise acelerar, resultado: 1
 # caso o carro precise desacelarar, resultado: -1
-# caso o carro precise manter a velocidade, resultado: 
+# caso o carro precise manter a velocidade, resultado: 0
 
 traseira_A = int(input())
 traseira_B = int(input())
